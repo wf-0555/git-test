785362253041d2f85bc3d61c4c7fc212ed8e7a7d
watfox <1477289537@gmail.com>
2022-08-13 16:13:42 +0800

    second commit

--- src/main/java/com/test/git/test.java
+++ src/main/java/com/test/git/test.java
@@ -1,7 +1,12 @@
 package com.test.git;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
-    public static void main(String[] args) {
+    public static  void main(String[] args) {
         System.out.println("hello");
+        List<Integer> list=new ArrayList<>();
+        System.out.println(list);
     }
 }
